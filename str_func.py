75441966ff2398a18de4c5bb603a9da87debb576
vovai <vova.izmodenov@mail.ru>
2023-03-30 22:45:13 +0300

    add new func and docstring for 1 func and new func

--- str_func.py
+++ str_func.py
@@ -2,3 +2,14 @@ def capitalize_str(a):
     b = a.capitalize()
     return b
 
+"""
+Функция переводит слова в верхний регистр
+"""
+
+def title_str(a):
+    b = a.title()
+    return b
+
+"""
+Функция начинает каждое слово с заглавной буквы
+"""
